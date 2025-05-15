005ccd19577a0b19fe6a50984203658818f0fbbb
111111162karan <67959293+111111162karan@users.noreply.github.com>
2025-05-15 13:46:31 +0200

    Update Food.cs
    completetd some methods

--- CookingCraft/CookingCraft/Food.cs
+++ CookingCraft/CookingCraft/Food.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Ribbon.Primitives;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace CookingCraft
 {
@@ -12,23 +17,33 @@ namespace CookingCraft
     {
         // Properties
 
+        private const int PixelSize = 32; // Size of the sprite in pixels
+        private const int SpritesPerRow = 8; // Number of sprites per row in the sprite sheet
+
         public string Name { get; set; }
         public int ID { get; set; }
-        private Canvas Sprite { get; set; }
+        private ImageSource Sprite { get; set; }
         private Canvas GameCanvas { get; set; }
 
+        private ObservableCollection<Food> CollectionFood { get; set; }
+
         // Constructor
-        public Food(int id, Canvas gameCanvas, bool bought = false)
+        public Food(int id, Canvas gameCanvas, ObservableCollection<Food> collection, bool bought = false)
         {
             // bought decides whether the Ingredient was bought, combined or available since the start
-            
+
             ID = id;
             GameCanvas = gameCanvas;
+            CollectionFood = collection;
+            // Load the sprite and name
+            this.LoadSprite();
+            this.LoadName();
 
             if (bought)
             {
 
 
+                this.AddEntry(CollectionFood);
             }
 
         }
@@ -48,7 +63,29 @@ namespace CookingCraft
         public Food Combine(Food Ingredient)
         {
             // Ingredient : Food that this Object gets combined with
-            Food food = new Food();
+            Food food = null;
+
+            // Decide which ID is smaller (becuase the CSV is sorted)
+            int smallerID = Math.Min(ID, Ingredient.ID);
+            int biggerID = Math.Max(ID, Ingredient.ID);
+
+
+            using (var reader = new StreamReader("Ressources/IngredientCombines.csv"))
+            {
+                // Read the file line by line
+                string? line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    // Split the line into parts
+                    string[] parts = line.Split(';');
+                    // Check if the ID matches
+                    if (int.Parse(parts[0]) == smallerID && int.Parse(parts[1]) == biggerID)
+                    {
+                        food = new Food(int.Parse(parts[3]), GameCanvas, CollectionFood);
+                        break;
+                    }
+                }
+            }
 
 
 
@@ -65,21 +102,37 @@ namespace CookingCraft
         // Sets the Sprite
         private void LoadSprite()
         {
+            var spriteSheet = new BitmapImage(new Uri("Ressources/IngredientsSpriteSheet.png"));
+
+            int spriteIndex = ID - 1; // Assuming ID starts from 1
+            int spriteX = (spriteIndex % SpritesPerRow) * PixelSize;
+            int spriteY = (spriteIndex / SpritesPerRow) * PixelSize;
+            var spriteRect = new Int32Rect(spriteX, spriteY, PixelSize, PixelSize);
+            var croppedSprite = new CroppedBitmap(spriteSheet, spriteRect);
+            Sprite = croppedSprite;
 
         }
 
         // Sets the Name 
         private void LoadName()
         {
+            using (var reader = new StreamReader("Ressources/IngredientNames.csv"))
+            {
+                // Read the file line by line
+                string? line; ;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    // Split the line into parts
+                    string[] parts = line.Split(';');
+                    // Check if the ID matches
+                    if (int.Parse(parts[0]) == ID)
+                    {
+                        Name = parts[1];
+                        break;
+                    }
+                }
+            }
 
         }
-
-        // Call this if Food is created or placed somewehere
-        private void Pop()
-        {
-
-        }
-
-
     }
 }
