7a314441292f75fe53e133f15f0c3dfcd35910fb
111111162karan <67959293+111111162karan@users.noreply.github.com>
2025-05-21 16:07:30 +0200

    Game Class
    Made a few methods

--- CookingCraft/CookingCraft/Game.cs
+++ CookingCraft/CookingCraft/Game.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
@@ -10,13 +14,16 @@ namespace CookingCraft
 {
     public class Game
     {
-        public List<Food> Ingredients { get; set; } // List to hold the ingredients
-        public int Coins { get; set; } // Coins the player has
+        public List<int> IngredientIDs { get; set; } = new List<int>();
+        public List<int> AchievmentIDs { get; set; } = new List<int>();
+        public int Coins { get; set; }
+        public string KitchenName { get; set; } = "CookiesKitchen";
+        private static string GetSavePath(string SaveName) => $"Saves/{SaveName}.json";
 
 
-        public Game(List<Food> ingredients, int coins) 
+        public Game(List<int> ingredients, int coins) 
         {
-            Ingredients = ingredients;
+            IngredientIDs = ingredients;
             Coins = coins;
 
         }
@@ -24,10 +31,37 @@ namespace CookingCraft
         {
             
         }
-        public static Game LoadGame(string FileName)
+        public void Save(string FileName)
+        {
+
+            var json = JsonSerializer.Serialize(this, new JsonSerializerOptions { WriteIndented = true });
+
+            using (var writer = new StreamWriter(GetSavePath(FileName)))
+            {
+                writer.Write(json);
+
+            }
+            
+        }
+        public static Game Load(string FileName)
         {
             Game game = new Game();
+            using (var reader = new StreamReader(GetSavePath(FileName)))
+            {
+                var json = reader.ReadToEnd();
+                game = JsonSerializer.Deserialize<Game>(json);
+            }
             return game;
+        }
+        public void Initialise(ObservableCollection<Food> Entrys, Canvas GameCanvas)
+        {
+            // Load the ingredients from the IDs
+            foreach (int id in IngredientIDs)
+            {
+                Food food = new Food(id, GameCanvas,Entrys, bought:true);
+
+            }
+
         }
     }
 }
